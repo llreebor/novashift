09d620ce7b552fc918f470987797667a2b32e161
llreebor <timoschuk.roman@gmail.com>
2025-11-21 17:17:43 +0200

    fix images size

--- app/about-us/sections/hero.tsx
+++ app/about-us/sections/hero.tsx
@@ -21,16 +21,6 @@ export default function HeroSection() {
 							height={573}
 						/>
 					</div>
-
-					{/* <div className='absolute bottom-0 shrink-0 max-w-[546px] mx-auto left-[-100px] right-[-100px] md:left-[unset] mx:m-0 lg:max-w-[600px] lg:-right-28 lg:h-auto xl:-right-[200px] xl:max-w-[800px] 2xl:max-w-[861px] 2xl:-right-[360px]'>
-						<Image
-							className='aspect-3/2 '
-							src={"/images/about/hero-image.png"}
-							alt='hero image'
-							width={861}
-							height={573}
-						/>
-					</div> */}
 				</div>
 			</div>
 		</section>
